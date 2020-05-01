dbb78fc26672b8ba478bdfac40b1ccd80ca6ebb6
vnze <vaughnnze@gmail.com>
2020-04-30 18:02:42 -0700

    first commit

---
+++ src/main/java/com/kasperin/inventory_management/InventoryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.kasperin.inventory_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kasperin/inventory_management/InventoryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kasperin.inventory_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
