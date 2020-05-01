d9377f51a45011f559cf0b917bd23b523ffd79ef
chace <chacewell@gmail.com>
2020-04-30 18:12:48 -0700

    Sent Von Kasperin

---
+++ src/main/java/com/kasperin/inventory_management/HelloKasper.java
@@ -0,0 +1,14 @@
+package com.kasperin.inventory_management;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class HelloKasper implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("Hello Kasper!");
+    }
+}
