f381fe559c2264139239987cc5515b3214352ff6
vnze <vaughnnze@gmail.com>
2020-05-01 21:47:07 -0700

    Added Produce Mapper And Model Package for API

--- src/main/java/com/kasperin/inventory_management/api/v1/mapper/FruitAndVegeMapper.java
+++ src/main/java/com/kasperin/inventory_management/api/v1/mapper/FruitAndVegeMapper.java
@@ -1,4 +1,15 @@
 package com.kasperin.inventory_management.api.v1.mapper;
 
+import com.kasperin.inventory_management.api.v1.model.FruitAndVegeDTO;
+import com.kasperin.inventory_management.domain.FruitAndVege;
+import org.mapstruct.factory.Mappers;
+
 public interface FruitAndVegeMapper {
+
+    FruitAndVegeMapper INSTANCE = Mappers.getMapper(FruitAndVegeMapper.class);
+
+    FruitAndVegeDTO fruitAndVegeToFruitAndVegeDTO(FruitAndVege fruitAndVege);
+
+    FruitAndVege fruitAndVegeDTOtoFruitAndVege(FruitAndVegeDTO fruitAndVegeDTO);
+
 }
