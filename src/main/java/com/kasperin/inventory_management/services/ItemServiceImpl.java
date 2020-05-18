a38185f8275b88a7ad10e7184aeaf813b05c2237
vnze <vaughnnze@gmail.com>
2020-05-18 09:56:56 -0700

    added github link and license to swagger config

--- src/main/java/com/kasperin/inventory_management/services/ItemServiceImpl.java
+++ src/main/java/com/kasperin/inventory_management/services/ItemServiceImpl.java
@@ -1,12 +1,5 @@
 package com.kasperin.inventory_management.services;
 
-import com.kasperin.inventory_management.controllers.v1.FruitAndVegeController;
-import com.kasperin.inventory_management.controllers.v1.ProcessedFoodController;
-import com.kasperin.inventory_management.controllers.v1.StationaryController;
-
-import com.kasperin.inventory_management.repository.FruitAndVegeRepository;
-import com.kasperin.inventory_management.repository.ProcessedFoodRepo;
-import com.kasperin.inventory_management.repository.StationaryRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -35,16 +28,3 @@ public class ItemServiceImpl implements ItemService {
     }
 
 }
-
-
-  /*  private String getFruitAndVegeUrl(Long id) {
-        return FruitAndVegeController.BASE_URL + "/id/" + id;
-    }
-
-    private String getProcessedFoodUrl(Long id) {
-        return ProcessedFoodController.BASE_URL + "/id/" + id;
-    }
-
-    private String getStationaryUrl(Long id) {
-        return StationaryController.BASE_URL + "/id/" + id;
-    }*/
